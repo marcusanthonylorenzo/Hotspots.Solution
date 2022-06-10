01c6579bfeeda7d43dfa19eeb21c46903735bc5c
Marcus <marcus.kaitara.lorenzo@gmail.com>
2022-06-10 09:39:28 -0700

    apply CRUD to restaurants

---
+++ Hotspots/Controllers/RestaurantsController.cs
@@ -0,0 +1,123 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Hotspots.Models;
+
+namespace Hotspots.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class RestaurantsController : ControllerBase
+  {
+    private readonly HotspotsContext _db;
+
+    public RestaurantsController(HotspotsContext db)
+    {
+      _db = db;
+    }
+
+    // GET: https://localhost:5001/api/Restaurants
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Restaurant>>> Get(string name, string city, string cuisine)
+    {
+      var query = _db.Restaurants.AsQueryable();
+
+      if (name != null)
+      {
+        query = query.Where(entry => entry.Name == name);
+      }
+
+      if (city != null)
+      {
+        query = query.Where(entry => entry.City == city);
+      }    
+
+      if (cuisine != null)
+      {
+        query = query.Where(entry => entry.Cuisine == cuisine);
+      }      
+
+      return await query.ToListAsync();
+    }
+
+    // GET: https://localhost:5001/api/Restaurants/{id}
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Restaurant>> GetRestaurant(int id)
+    {
+        var Restaurant = await _db.Restaurants.FindAsync(id);
+
+        if (Restaurant == null)
+        {
+            return NotFound();
+        }
+
+        return Restaurant;
+    }
+
+    // PUT: https://localhost:5001/api/Restaurants/{id}
+    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Put(int id, Restaurant restaurant)
+    {
+      if (id != restaurant.Id)
+      {
+        return BadRequest();
+      }
+
+      _db.Entry(restaurant).State = EntityState.Modified;
+
+      try
+      {
+        await _db.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if (!RestaurantExists(id))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+
+      return NoContent();
+    }
+
+    // POST: api/Restaurants
+    [HttpPost]
+    public async Task<ActionResult<Restaurant>> Post(Restaurant restaurant)
+    {
+      _db.Restaurants.Add(restaurant);
+      await _db.SaveChangesAsync();
+
+      return CreatedAtAction(nameof(GetRestaurant), new { id = restaurant.Id }, restaurant);
+    }
+
+    // DELETE: api/Restaurants/5
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteRestaurant(int id)
+    {
+      var restaurant = await _db.Restaurants.FindAsync(id);
+      if (restaurant == null)
+      {
+        return NotFound();
+      }
+
+      _db.Restaurants.Remove(restaurant);
+      await _db.SaveChangesAsync();
+
+      return NoContent();
+    }
+
+    private bool RestaurantExists(int id)
+    {
+      return _db.Restaurants.Any(e => e.Id == id);
+    }
+  }
+}
